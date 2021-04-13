c6933a49ec2bc1d1d4fc700fbdcf27360b911214
Your Name <su7265168@gmail.com>
2021-04-13 15:46:41 +0530

    Login done

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use App\Models\User;
+class UserController extends Controller
+{
+    function login(Request $request){
+    	$user = User::where(['email'=>$request->email])->first();
+    	if(!$user || !Hash::check($request->password,$user->password)){
+    		return "User or Password  is not matched";
+    	}else{
+    		$request->session()->put('user',$user);
+    		return redirect('/');
+    	}
+    }
+}
